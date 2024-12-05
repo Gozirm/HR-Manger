42dab30841296d2959fac08b12d4536ed221a796
Chigozirim Joshua <divinefavourjoshua03@gmail.com>
2024-12-05 17:42:34 +0100

    intial Init

--- front-end/src/Auth/CheckEmail.jsx
+++ front-end/src/Auth/CheckEmail.jsx
@@ -69,7 +69,7 @@ const CheckEmail = () => {
       }
       if (res.success) {
         toast.success(res.message);
-        navigate("");
+        navigate("https://hr-manger.vercel.app");
       }
     } catch (error) {
     } finally {
